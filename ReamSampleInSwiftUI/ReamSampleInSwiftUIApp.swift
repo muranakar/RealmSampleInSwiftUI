9832d217193e919d685efae5044aa36da0f9abf2
muranakar <P1211065@gmail.com>
2023-11-20 15:57:34 +0900

    Initial Commit

---
+++ ReamSampleInSwiftUI/ReamSampleInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReamSampleInSwiftUIApp.swift
+//  ReamSampleInSwiftUI
+//
+//  Created by ryo muranaka on 2023/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ReamSampleInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
